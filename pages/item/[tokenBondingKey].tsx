278693abd7d2dfb410a986cab644a5008c7bf4fb
Chewing Glass <chewinglass123@gmail.com>
2022-01-30 13:42:24 -0600

    Bigger max width

--- pages/item/[tokenBondingKey].tsx
+++ pages/item/[tokenBondingKey].tsx
@@ -69,7 +69,7 @@ export const MarketDisplay: NextPage = ({ name, image, description }: InferGetSe
     </Head>
     <Box w="full" h="full" overflow="auto" paddingTop={{ sm: "18px" }}>
       <Center flexGrow={1}>
-        <Center bg="white" shadow="xl" rounded="lg" w="500px">
+        <Center bg="white" shadow="xl" rounded="lg" maxW="600px">
           <MarketplaceItem 
             name={name}
             description={description}
